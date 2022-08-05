bf1725dc4be915f5a125ee7003ec23c749a20ecd
Sameer Nyaupane <sameernyaupane@outlook.com>
2022-08-05 13:15:06 +0545

    Add changes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +20,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+Route::post('register', [RegisteredUserController::class, 'store']);
+
+Route::middleware('auth:sanctum')->get('/logout', function (Request $request) {
+    if($request->user()->currentAccessToken()->delete()) {
+        return response()->json(["success" => true]);
+    }
+
+    return response()->json(["success" => false]);
 });
